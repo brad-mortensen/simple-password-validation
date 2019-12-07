c8b96e826cdf17ab5ece0bb2682157e68d66ecf3
Brad Mortensen <b.mort86@gmail.com>
2019-12-06 21:16:55 -0600

    adds validation

---
+++ index.js
@@ -0,0 +1,17 @@
+let input = document.querySelector('.password');
+let greater = document.querySelector('.greater');
+let less = document.querySelector('.less');
+let special = document.querySelector('.special');
+let number = document.querySelector('.number');
+
+const show = () => {
+  input.type === 'password' ? input.type = 'text' : input.type = 'password';
+}
+
+const validate = () => {
+  let value = input.value, length = value.length;
+  length > 5 ? greater.style.color = 'green' : greater.style.color = 'tomato';
+  length < 20 ? less.style.color = 'green' : less.style.color = 'tomato';
+  value.match(/[!@#$%^&*(),.?":{}|<>]/g) ? special.style.color = 'green' : special.style.color = 'tomato';
+  value.match(/[0-9]/g) ? number.style.color = 'green' : number.style.color = 'tomato';
+};
